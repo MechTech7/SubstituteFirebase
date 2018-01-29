ea06c06595f700672a873dda76913ab3bffc2e26
Mason <masontyler747@gmail.com>
2018-01-29 13:47:26 -0500

    Just some random stuff

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,59 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// [START all]
+// [START import]
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+// [END import]
 
-exports.makeUppercase = functions.database.ref('/substitute_teachers/johndoe_uid/firstname')
+// [START addMessage]
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+// [START addMessageTrigger]
+// [END addMessage]
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  var a = admin.database().ref('/substituteteachers/johndoe_uid/firstname').push({original: original})
+  a.then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref);
+    return 0
+  }).catch(function(error){
+	   console.log(error);
+  });
+
+});
+
+// [START makeUppercase]
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+// [START makeUppercaseTrigger]
+exports.makeUppercase = functions.database.ref('/substituteteachers/johndoe_uid/firstname')
     .onWrite(event => {
+// [END makeUppercaseTrigger]
+      // [START makeUppercaseBody]
       // Grab the current value of what was written to the Realtime Database.
       const original = event.data.val();
       console.log('Uppercasing', event.params.pushId, original);
@@ -17,5 +62,7 @@ exports.makeUppercase = functions.database.ref('/substitute_teachers/johndoe_uid
       // writing to the Firebase Realtime Database.
       // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
       return event.data.ref.parent.child('uppercase').set(uppercase);
+      // [END makeUppercaseBody]
     });
-
+// [END makeUppercase]
+// [END all]
